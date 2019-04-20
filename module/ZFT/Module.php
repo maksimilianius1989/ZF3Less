3000f71a77a94435e334785a9e062cfe7bf69e63
vimax <it.vimax@gmail.com>
2019-04-20 20:54:20 +0300

    feature/Lessons3 Added xdebug

--- module/ZFT/Module.php
+++ module/ZFT/Module.php
@@ -8,12 +8,10 @@
 
 namespace ZFT;
 
-
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
 
 class Module implements ServiceProviderInterface
 {
-
     /**
      * Expected to return \Zend\ServiceManager\Config object or array to
      * seed such an object.
---
+++ module/ZFT/src/User.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ZFT;
+
+class User
+{
+
+}
