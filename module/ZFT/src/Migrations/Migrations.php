772c97731951ae204c2bd0869154bb6b78933d40
Bukach_M <it.vimax@gmail.com>
2019-05-04 15:56:49 +0300

    Run database migrations from current schema version up to MINIMUM_SCHEMA_VERSION

--- module/ZFT/src/Migrations/Migrations.php
+++ module/ZFT/src/Migrations/Migrations.php
@@ -11,11 +11,12 @@ use Zend\Db\Sql\Ddl\Column\Varchar;
 use Zend\Db\Sql\Ddl\CreateTable;
 use Zend\Db\Sql\Ddl\SqlInterface;
 use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Where;
 
 class Migrations
 {
     const MINIMUM_SCHEMA_VERSION = 1;
-    const INI_TABLE = 'ini-dev';
+    const INI_TABLE = 'ini';
 
     /** @var Adapter */
     private $adapter;
@@ -58,11 +59,6 @@ class Migrations
             return 0;
         }
 
-//        $sql = 'SELECT value FROM ' . $this->platform->quoteIdentifier(self::INI_TABLE) . ' WHERE option = :option';
-//        $result = $this->adapter->query($sql, ['option' => 'ZftSchemaVersion']);
-//        $result = $result->toArray();
-//        $version = $result[0]['value'];
-
         $sql = new Sql($this->adapter);
         $select = $sql->select();
         $select->from(self::INI_TABLE);
@@ -76,9 +72,47 @@ class Migrations
         return $version;
     }
     
-    protected function update1_001 ()
+    public function run()
     {
-        $iniTable = new CreateTable('ini');
+        $migrationClass = new \ReflectionClass(Migrations::class);
+        $methods = $migrationClass->getMethods(\ReflectionMethod::IS_PROTECTED);
+
+        $updates = [];
+        array_walk($methods, function (\ReflectionMethod $method) use (&$updates) {
+            $version = (int) substr($method->getName(), strpos($method->getName(), '_') + 1);
+            $updates[$version] = $method->getName();
+        });
+
+        ksort($updates);
+
+        $currentVersion = (int) $this->getVersion();
+        for ($v = $currentVersion + 1; $v <= self::MINIMUM_SCHEMA_VERSION; $v++) {
+            $update = $updates[$v];
+            $this->{$update}();
+
+            $this->setVersion($v);
+        }
+
+        return;
+    }
+    
+    private function setVersion($version)
+    {
+        $sql = new Sql($this->adapter);
+        $schemaVersionUpdate = $sql->update();
+        $schemaVersionUpdate->table(self::INI_TABLE);
+        $schemaVersionUpdate->set(['value' => $version]);
+
+        $schemaVersionRow = new Where();
+        $schemaVersionRow->equalTo('optoin', 'ZftSchemaVersion');
+
+        $schemaVersionStatement = $sql->prepareStatementForSqlObject($schemaVersionUpdate);
+        $schemaVersionStatement->execute();
+    }
+    
+    protected function update_001 ()
+    {
+        $iniTable = new CreateTable(self::INI_TABLE);
 
         $option = new Varchar('option');
         $value = new Varchar('value');
@@ -95,7 +129,7 @@ class Migrations
             'option' => 'ZftSchemaVersion',
             'value' => 1,
         ];
-        $insertInitialVersion->columns(array_column($value));
+        $insertInitialVersion->columns(array_keys($value));
         $insertInitialVersion->values(array_values($value));
 
         $insertStatement = $sql->prepareStatementForSqlObject($insertInitialVersion);
