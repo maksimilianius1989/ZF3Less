72e136e078d993d9bd4a6ac1954a62940f262669
Bukach_M <it.vimax@gmail.com>
2019-05-13 00:13:54 +0300

    Added Flash message plugin

--- module/Portal/src/Controller/ProfileController.php
+++ module/Portal/src/Controller/ProfileController.php
@@ -3,27 +3,36 @@
 namespace Portal\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Mvc\Plugin\FlashMessenger\FlashMessenger;
+
+/**
+ * @method FlashMessenger flashMessenger()
+ */
 
 class ProfileController extends AbstractActionController
 {
     public function viewAction()
     {
-
+        return;
     }
 
     public function editAction()
     {
-
+        return;
     }
 
     public function submitAction()
     {
-        $success = true;
+        $flashMessenger = $this->flashMessenger();
 
+        $success = true;
         if ($success) {
             $destinationRoute = 'profile/view_profile';
+            $flashMessenger->addSuccessMessage('Profile successfully stored.');
         } else {
             $destinationRoute = 'profile/edit_profile/form_profile';
+            $flashMessenger->addErrorMessage('Invalid email');
+            $flashMessenger->addErrorMessage('Invalid username');
         }
 
         return $this->redirect()->toRoute($destinationRoute);
